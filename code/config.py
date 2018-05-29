014385df4a3f231b94b77807862b90781f890c5b
redekok <R.E.deKok@student.tudelft.nl>
2018-05-29 10:22:51 +0200

    Delete old files

--- code/config.py
+++ code/config.py
@@ -1,6 +1,42 @@
-config = {
+# Instagram API
+instagram_config = {
     'client_id': '81883488b84d4b98826ebfee9c161083',
     'client_secret': '91b97e2bfaee4e639c35b4d9c2ee0882',
     'access_token': '2094437586.8188348.a8beec9a7df449da961eb2810298b55a',
     'redirect_uri': 'http://localhost:8515/oauth_callback'
+}
+
+# Twitter API
+twitter_config = {
+    'consumer_key': 'huvgmNpGMk7sqGzXgojrbmUbE',
+    'consumer_secret': '49ObbSKkfltqkahcxoGKwvV32y0UCgCInY1eek19Iv8cK9YFyP',
+    'access_token': '790524240307089408-QP6v1qDgeyxO9mctVUJHbrL22XoiomT',
+    'access_token_secret': 'kKGis1U13kaQKAfHJ7kaS4VG279Xt536pQSUjvcg6RVJZ'
+}
+
+# Facebook Graph API
+facebook_config = {
+    'app_id': '253299941905863',
+    'app_secret': 'ed940dbde2751fa0c5c3502518ba10c3',
+    'app_code': '253299941905863|I8q96FHiFB56zQe1DfI_VwwWnjg',
+    'user_token': 'EAADmXZC0PFccBAMZBnThAvrIxiOKfjUtcVeSD7gTswqAfignp31n8t0Xq8bfO93Tq3RvAewmfE3Vm2b4Rdz8WhZCIH9LFy6GxUHdQ6c9B4SKhcduo9oCqbUbREoYbk52omNWWpZBOhUXcKOC8OZBK5otd7Wa5waWWJRwEqhmz75xZAa9TRH9tDEqyR1oO0b7EZD',
+    'short_lived_access_token': 'EAACEdEose0cBABiOOLBMjUhblMc8mxggTIl9w4YqDrN3AZAhs0zCZCw0m02TGGwB83aDhxQgefqUc3K1QMt3Qucenys7Gwr8nxjkae37pZBfIpcTHvIyZBgeU8E4t1PGvia8aGa33mlmxnaxn4gLApJKRWSdo6316looddUasOhm4SbLwiU9dZAZAUFl8dIBI9hHZCvokYD8AZDZD',
+    'extended_token': {
+        'access_token': 'EAADmXZC0PFccBAKZByyxcJmBIoZBMdku3DHSuFBtZBPefOTZBMZAu2rBD1vkhDWBao95AfoechFKVSypp9czCdoZCgnbaEvDqA9PaUx3ZCGZAhBa7SJat3Hbe3qKMd3pAhXvZCFkAx7Wwy0sW6Aqh3AQc0m8IZBwrH4F5An5y02VqgsqgZDZD',
+        'token_type': 'bearer',
+        'expires_in': 5184136
+    },
+    'extended_on': 1527023579
+}
+
+# Google Places API
+google_config = {
+    'api_key': 'AIzaSyBSvDfYxtbKbAtMn9H7zxKRoe-gD3PEaMo'
+}
+
+# MongoDB
+mongo_config = {
+    'DB_HOST': 'localhost',
+    'DB_PORT': 27017,
+    'DB_NAME': 'SSM'
 }
